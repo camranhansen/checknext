aeb88c9b9d6de5703d576d52007668ba20e96992
Camran Hansen <camran.hansen@gmail.com>
2019-06-22 18:25:34 -0400

    pick 4 arbritrary points

--- contours.py
+++ contours.py
@@ -19,22 +19,25 @@ mask = cv2.inRange(hsv, lower_val, upper_val)
 # apply mask to original image
 res = cv2.bitwise_and(img, img, mask= mask)
 #show imag
-cv2.imshow("Result", res)
+# cv2.imshow("Result", res)
 # detect contours in image
 contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # draw filled contour on result
+# for cnt in contours:
+#     print(cnt.min())
+
+#for contour 0
+
 for cnt in contours:
-    cv2.drawContours(res, [cnt], 0, (0,0,255), 2)
+    print(cnt[0])
 # detect edges in mask
-edges = cv2.Canny(mask,100,100)
+# edges = cv2.Canny(mask,100,100)
 # to save an image use cv2.imwrite('filename.png',img)
 #show images
-cv2.imshow("Result_with_contours", res)
-cv2.imshow("Mask", mask)
-cv2.imshow("Edges", edges)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-print(contours)
+# cv2.imshow("Result_with_contours", res)
+# cv2.imshow("Mask", mask)
+# cv2.imshow("Edges", edges)
+
 
 # gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 # gray = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -45,13 +48,15 @@ print(contours)
 # test_thing = cv2.erode(test_thing, None)
 # contours, hierarchy = cv2.findContours(test_thing, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 #
-rc = cv2.minAreaRect(contours[0])
-box = cv2.boxPoints(rc)
-for p in box:
-    pt = (p[0], p[1])
-    print(pt)
+# rect = cv2.minAreaRect(contours[1])
+# box = cv2.boxPoints(rect)
+# box = np.int0(box)
+# cv2.drawContours(img,[box],0,(0,255,0),2)
+# cv2.imshow("test",img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-cv2.imshow("plank", im)
+
 cv2.waitKey(0)
 
 
