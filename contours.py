1a2d82dd0c6e1572987d0213e0414ab47d5e83f6
Camran Hansen <camran.hansen@gmail.com>
2019-06-22 22:10:33 -0400

    pretiffied into functoin

--- contours.py
+++ contours.py
@@ -1,91 +1,80 @@
 import cv2
 import numpy as np
-import random
+
+def squarify(location):
+    img = cv2.imread(location)
 
 
-img = cv2.imread("small_1.jpg")
+    img = cv2.GaussianBlur(img, (5, 5),5)
+    # print(type(img))
+    # convert to HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-img = cv2.GaussianBlur(img, (5, 5),5)
+    # set lower and upper color limits (for pink)
+    lower_val = (131, 37, 180)
+    upper_val = (255, 161, 255)
 
-# convert to HSV
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_val, upper_val)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# set lower and upper color limits (for pink)
-lower_val = (131, 37, 180)
-upper_val = (255, 161, 255)
+    # print(len(contours))
+    points = []
 
-mask = cv2.inRange(hsv, lower_val, upper_val)
-contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for i in range(len(contours)):
+        totalx = 0
+        totaly = 0
+        for element in contours[i]:
+            totalx += element[0][0]
+            totaly += element[0][1]
 
-print(len(contours))
-points = []
+        points.append([int(totalx/len(contours[i])), int(totaly/len(contours[i]))])
 
-for i in range(len(contours)):
-    totalx = 0
-    totaly = 0
-    for element in contours[i]:
-        totalx += element[0][0]
-        totaly += element[0][1]
+    cnt = np.array(points)
 
-    points.append([int(totalx/len(contours[i])), int(totaly/len(contours[i]))])
+    rect = cv2.minAreaRect(cnt)
+    # print(rect)
+    # print(img.shape)
 
-
-# for cont in contours:
-    # x = random.randint(0, )
-    # points.append([cont[0][0][0], cont[0][0][1]])
-    # print([cont[0][0][0], cont[0][0][1]])
-
-# cv2.drawContours(img, contours, -1, (0,255, 0), 100)
-# res = cv2.resize(img, (600,600))
-# print(res[0,0])
-# cv2.imshow("contours", res)
-# cv2.waitKey(0)
-# print(points)
-cnt = np.array(points)
-
-rect = cv2.minAreaRect(cnt)
-# print(rect)
-print(img.shape)
-
-box = cv2.boxPoints(rect)
-box = np.int0(box)
-# cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    # cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
 
 
-width = int(rect[1][0])
-height = int(rect[1][1])
+    width = int(rect[1][0])
+    height = int(rect[1][1])
 
-src_pts = box.astype("float32")
+    src_pts = box.astype("float32")
 
-dst_pts = np.array([[0, height-1],
-                    [0, 0],
-                    [width-1, 0],
-                    [width-1, height-1]], dtype="float32")
+    dst_pts = np.array([[0, height-1],
+                        [0, 0],
+                        [width-1, 0],
+                        [width-1, height-1]], dtype="float32")
 
-M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
 
-warped = cv2.warpPerspective(img, M, (width, height))
+    warped = cv2.warpPerspective(img, M, (width, height))
 
-res = cv2.resize(warped, (800,800))
+    squared_image = cv2.resize(warped, (800,800))
 
 
-board = {}
+    return squared_image
 
-for x in range(0, 801, 100):
-    for y in range(0, 801, 100):
-        board[(x,y)] = "E"
 
-print(board)
-board[(0,0)] = "B"
-print(board)
+def make_board():
+    board = {}
 
-for pos in board:
-    cv2.circle(res,pos, 3, (0,255, 0), 3)
+    for x in range(0, 801, 100):
+        for y in range(0, 801, 100):
+            board[(x,y)] = "E"
+
+    return board
+
+# for pos in board:
+#     cv2.circle(res,pos, 3, (0,255, 0), 3)
 
 
 #
-cv2.imshow("test", res)
-# #
+cv2.imshow("test", squarify("small_1.jpg"))
 #
 cv2.waitKey(0)
 
