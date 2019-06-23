4c00f30b66e7e4c669bfcc8c5130dd95a154c947
Camran Hansen <camran.hansen@gmail.com>
2019-06-22 21:15:54 -0400

    added averaging algorithm for contours

--- contours.py
+++ contours.py
@@ -18,55 +18,59 @@ mask = cv2.inRange(hsv, lower_val, upper_val)
 contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 print(len(contours))
+points = []
+
+for i in range(len(contours)):
+    totalx = 0
+    totaly = 0
+    for element in contours[i]:
+        totalx += element[0][0]
+        totaly += element[0][1]
+
+    points.append([int(totalx/len(contours[i])), int(totaly/len(contours[i]))])
 
 
-# points = []
 # for cont in contours:
-#     # x = random.randint(0, )
-#     points.append([cont[0][0][0], cont[0][0][1]])
-#
+    # x = random.randint(0, )
+    # points.append([cont[0][0][0], cont[0][0][1]])
+    # print([cont[0][0][0], cont[0][0][1]])
+
 # cv2.drawContours(img, contours, -1, (0,255, 0), 100)
 # res = cv2.resize(img, (600,600))
 # print(res[0,0])
 # cv2.imshow("contours", res)
 # cv2.waitKey(0)
-# # print(points)
-# cnt = np.array(points)
-#
-# rect = cv2.minAreaRect(cnt)
-# # print(rect)
-# print(img.shape)
-#
-# box = cv2.boxPoints(rect)
-# box = np.int0(box)
+# print(points)
+cnt = np.array(points)
+
+rect = cv2.minAreaRect(cnt)
+# print(rect)
+print(img.shape)
+
+box = cv2.boxPoints(rect)
+box = np.int0(box)
 # cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
-#
-# # points = [[contours[0][0][0][0], contours[0][0][0][0]],
-# #           [contours[1][0][0][0], contours[1][0][0][0]],
-# #           [contours[2][0][0], contours[2][0][1]],
-# #           [contours[3][0][0], contours[3][0][1]]]
-# # note: can change this in the future to be more accurate
-#
-# width = int(rect[1][0])
-# height = int(rect[1][1])
-#
-# src_pts = box.astype("float32")
-#
-# dst_pts = np.array([[0, height-1],
-#                     [0, 0],
-#                     [width-1, 0],
-#                     [width-1, height-1]], dtype="float32")
-#
-# M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-#
-# warped = cv2.warpPerspective(img, M, (width, height))
-#
-# cv2.waitKey(0)
-# res = cv2.resize(warped, (300,300))
-# # cv2.imshow("original",img)
-# cv2.imshow("test", res)
-#
-# cv2.waitKey(0)
-#
-#
-#
+
+
+width = int(rect[1][0])
+height = int(rect[1][1])
+
+src_pts = box.astype("float32")
+
+dst_pts = np.array([[0, height-1],
+                    [0, 0],
+                    [width-1, 0],
+                    [width-1, height-1]], dtype="float32")
+
+M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+
+warped = cv2.warpPerspective(img, M, (width, height))
+
+res = cv2.resize(warped, (600,600))
+cv2.imshow("original", (cv2.resize(img, (600, 600))))
+cv2.imshow("test", res)
+
+cv2.waitKey(0)
+
+
+
